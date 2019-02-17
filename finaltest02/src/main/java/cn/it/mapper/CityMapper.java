bc6ce288e8011934faf87f5cf24e38852c884450
freak <cnfreak@163.com>
2019-02-17 10:44:44 +0800

    删除

--- finaltest02/src/main/java/cn/it/mapper/CityMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.it.mapper;
-
-import cn.it.domain.City;
-
-import java.util.List;
-
-public interface CityMapper {
-    List<City> findAll();
-}
--- finaltest02/src/main/java/cn/it/service/CityService.java
+++
@@ -1,9 +0,0 @@
-package cn.it.service;
-
-import cn.it.domain.City;
-
-import java.util.List;
-
-public interface CityService {
-    List<City> findAll();
-}
